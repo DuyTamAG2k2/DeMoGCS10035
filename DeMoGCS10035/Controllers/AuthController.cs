191418af08b015daaacab0abc31872b7143349b8
tran ky hung <trankyhung225@gmail.com>
2023-12-06 10:45:23 +0700

    login regist layout

---
+++ DeMoGCS10035/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeMoGCS10035.Controllers
+{
+    public class AuthController : Controller
+    {
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [Route("signup")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
